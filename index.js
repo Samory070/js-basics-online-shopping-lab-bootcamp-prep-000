25bc3c8d0c9fbfc50bb3b8f55ee6de27c8f15e89
samory harris <samory070@gmail.com>
2018-02-21 17:47:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,5 +72,8 @@ function removeFromCart(item) {
   return cart;
 }
 function placeOrder(cardNumber) {
+  if (!cardNumber) {
+    console.log("Sorry, we don't have a credit card on file for you")
+  }
 
 }
