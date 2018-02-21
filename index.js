ad179d54a2a9ecd01a9a912ff0b5b2e4e7f7fd1c
samory harris <samory070@gmail.com>
2018-02-21 17:10:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function viewCart() {
       itemsAndPrices[l-1] = 'and '.concat(itemsAndPrices[l-1]);
       itemsAndPrices = itemsAndPrices.join(', ');
   }
-  console.log()
+  console.log(`In your cart, you have ${itemsAndPrices}`)
 }
 
 function total() {
